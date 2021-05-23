4532bd942bb295a16b0dd7df52bc673b876c044d
Joao Victor Rodrigues <jvsr@uchicago.edu>
2021-05-23 01:55:47 -0500

    Added testing for get_imports.

--- test_scripts/xtract_python_test.py
+++ test_scripts/xtract_python_test.py
@@ -7,6 +7,22 @@ sys.path.append(path)
 import xtract_python_main as xpm
 
 class xtract_python_main_tests(unittest.TestCase):
+    def get_imports_test(self):
+        with open('get_imports_single_line.txt') as file:
+            expected = file.read()
+            output = str(xpm.get_imports(xpm.get_file_contents('single_line.py')))
+            assert expected == output, "single_line test failed."
+
+        with open('get_imports_multi_line.txt') as file:
+            expected = file.read()
+            output = str(xpm.get_imports(xpm.get_file_contents('multi_line.py')))
+            assert expected == output, "multi_line test failed."
+        
+        with open('get_imports_mixed_line.txt') as file:
+            expected = file.read()
+            output = str(xpm.get_imports(xpm.get_file_contents('single_line.py')))
+            assert expected == output, "single_line test failed."
+
     def get_functions_test(self):
         with open('get_functions_single_line.txt') as file:
             expected = file.read()
@@ -40,5 +56,7 @@ class xtract_python_main_tests(unittest.TestCase):
             assert expected == output
 
 test = xtract_python_main_tests()
+test.get_imports_test()
 test.get_functions_test()
 test.extract_python_test()
+
